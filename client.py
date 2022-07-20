894a4e82053ce284faeeb381f30f7c3d50d40fad
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-20 18:07:21 +0700

    delete tk

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import socket
 import argparse
 import os
 import sys
-import tkinter as tk
 
 
 class Send(threading.Thread):
@@ -67,7 +66,6 @@ class Receive(threading.Thread):
             if message:
 
                 if self.messages:
-                    self.messages.insert(tk.END, message)
                     print('hi')
                     print('\r{}\n{}: '.format(message, self.name), end = '')
                 
@@ -139,9 +137,7 @@ class Client:
             text_input(tk.Entry): A tk.Entry object meant for user text input.
         """
         message = text_input.get()
-        text_input.delete(0, tk.END)
-        self.messages.insert(tk.END, '{}: {}'.format(self.name, message))
-
+        
         # Type 'QUIT' to leave the chatroom
         if message == 'QUIT':
             self.sock.sendall('Server: {} has left the chat.'.format(self.name).encode('ascii'))
@@ -164,45 +160,6 @@ def main(host, port):
     """
     client = Client(host, port)
     receive = client.start()
-
-    window = tk.Tk()
-    window.title('Chatroom')
-
-    frm_messages = tk.Frame(master=window)
-    scrollbar = tk.Scrollbar(master=frm_messages)
-    messages = tk.Listbox(
-        master=frm_messages, 
-        yscrollcommand=scrollbar.set
-    )
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y, expand=False)
-    messages.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    
-    client.messages = messages
-    receive.messages = messages
-
-    frm_messages.grid(row=0, column=0, columnspan=2, sticky="nsew")
-
-    frm_entry = tk.Frame(master=window)
-    text_input = tk.Entry(master=frm_entry)
-    text_input.pack(fill=tk.BOTH, expand=True)
-    text_input.bind("<Return>", lambda x: client.send(text_input))
-    text_input.insert(0, "Your message here.")
-
-    btn_send = tk.Button(
-        master=window,
-        text='Send',
-        command=lambda: client.send(text_input)
-    )
-
-    frm_entry.grid(row=1, column=0, padx=10, sticky="ew")
-    btn_send.grid(row=1, column=1, pady=10, sticky="ew")
-
-    window.rowconfigure(0, minsize=500, weight=1)
-    window.rowconfigure(1, minsize=50, weight=0)
-    window.columnconfigure(0, minsize=500, weight=1)
-    window.columnconfigure(1, minsize=200, weight=0)
-
-    window.mainloop()
 
 
 if __name__ == '__main__':
